83ab7a16b99a0558e274eae2f585c5bf3cf2c83f
pablomiklas@gmail.com <pablomiklas@gmail.com>
2020-01-16 22:04:01 +0100

    added logout, logoutAll and authorization

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
@@ -15,13 +16,30 @@ router.post('/users', async (req, res) => {
 	}
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
 	try {
-		const users = await User.find({});
-		res.send(users);
+		req.user.tokens = req.user.tokens.filter(({ token }) => token !== req.token);
+		await req.user.save();
+
+		res.send('completed logout!');
 	} catch (error) {
-		res.status(500).send(error);
+		res.status(500).send();
 	}
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+	try {
+		req.user.tokens = [];
+		await req.user.save();
+
+		res.send();
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
+router.get('/users/me', auth, async (req, res) => {
+	res.send(req.user);
 });
 
 router.post('/users/login', async (req, res) => {
